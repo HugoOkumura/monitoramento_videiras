38883db534d2646ccdac45284f713d61e02946c0
HugoOkumura <hugo-naoki@hotmail.com>
2026-04-13 21:18:58 -0300

    Adicionando range de aceitação dos dados

--- models/models.py
+++ models/models.py
@@ -2,25 +2,30 @@
 MQTT_DATA = {
     "type" : "object",
     "properties": {
-        "sensor_id": {"type": "number"},
-        "temperature": {"type": "number"},
-        "humidity": {"type": "number"}
+        "sensor_id": {"type": ["number", "null"]},
+        "temperature": {"type": ["number", "null"]},
+        "air_humidity": {"type": ["number", "null"]}
     },
-    "required": ["sensor_id", "temperature", "humidity"],
+    "required": ["sensor_id", "temperature", "air_humidity"],
     "additionalProperties": False
 }
 
 INGESTED_DATA = {
     "type" : "object",
     "properties": {
-        "sensor_id": {"type": "number"},
-        "temperature": {"type": "number"},
-        "humidity": {"type": "number"},
+        "sensor_id": {"type": ["number", "null"]},
+        "temperature": {"type": ["number", "null"]},
+        "air_humidity": {"type": ["number", "null"]},
         "variety": {"type":"string"},
         "ingestion_timestamp": {
             "type":"string",
             "format":"date-time"            
         },
     },
-    "required": ["sensor_id","temperature","humidity","variety","ingestion_timestamp"]
+    "required": ["sensor_id","temperature","air_humidity","variety","ingestion_timestamp"]
+}
+
+VALUE_RANGES = {
+    "temperature": [-10,50],
+    "air_humidity": [0,100],
 }
